e90fc5eaa3b4235b2bbd1dcaae2d37cc838c5f19
Antonio Caggiano <info@antoniocaggiano.eu>
2022-07-18 00:26:36 +0200

    logic: Mux4Way16
    Introduce Signal2 and define the Mux4Way16 gate in terms of three Mux16.

---
+++ src/logic/mux4way16.rs
@@ -0,0 +1,95 @@
+// Copyright © 2022
+// Author: Antonio Caggiano <info@antoniocaggiano.eu>
+// SPDX-License-Identifier: MIT
+
+use super::{Mux16, Signal16, Signal2, Unit};
+
+/// 4-way 16-bit multiplexor
+#[derive(Default)]
+pub struct Mux4Way16 {
+    a: Signal16,
+    b: Signal16,
+    c: Signal16,
+    d: Signal16,
+    sel: Signal2,
+    out: Signal16,
+
+    mux_a_b: Mux16,
+    mux_c_d: Mux16,
+    mux_out: Mux16,
+}
+
+impl Mux4Way16 {
+    /// Carries out the simulation and returns
+    /// ```c
+    /// if sel == 0b00 { a }
+    /// if sel == 0b01 { b }
+    /// if sel == 0b10 { c }
+    /// if sel == 0b11 { d }
+    /// ```
+    pub fn sim(&mut self, signals: [Signal16; 4], sel: Signal2) -> Signal16 {
+        self.a = signals[0];
+        self.b = signals[1];
+        self.c = signals[2];
+        self.d = signals[3];
+        self.sel = sel;
+        self.eval();
+        self.out
+    }
+}
+
+impl Unit for Mux4Way16 {
+    fn eval(&mut self) {
+        let sel0 = self.sel.get(0).into();
+        let sel1 = self.sel.get(1).into();
+
+        self.out = self.mux_out.sim(
+            self.mux_a_b.sim(self.a, self.b, sel0),
+            self.mux_c_d.sim(self.c, self.d, sel0),
+            sel1,
+        );
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn run() {
+        let mut mux = Mux4Way16::default();
+
+        let signals = [Signal16::FALSE; 4];
+
+        mux.sim(signals, Signal2::new(0));
+        assert_eq!(mux.out, Signal16::FALSE);
+
+        mux.sim(signals, Signal2::new(1));
+        assert_eq!(mux.out, Signal16::FALSE);
+
+        mux.sim(signals, Signal2::new(2));
+        assert_eq!(mux.out, Signal16::FALSE);
+
+        mux.sim(signals, Signal2::new(3));
+        assert_eq!(mux.out, Signal16::FALSE);
+
+        let signals = [
+            Signal16::from(0b0001001000110100u16),
+            Signal16::from(0b1001100001110110u16),
+            Signal16::from(0b1010101010101010u16),
+            Signal16::from(0b0101010101010101u16),
+        ];
+
+        mux.sim(signals, Signal2::new(0));
+        assert_eq!(mux.out, mux.a);
+
+        mux.sim(signals, Signal2::new(1));
+        assert_eq!(mux.out, mux.b);
+
+        mux.sim(signals, Signal2::new(2));
+        assert_eq!(mux.out, mux.c);
+
+        mux.sim(signals, Signal2::new(3));
+        assert_eq!(mux.out, mux.d);
+    }
+}
---
+++ src/logic/signal2.rs
@@ -0,0 +1,113 @@
+// Copyright © 2022
+// Author: Antonio Caggiano <info@antoniocaggiano.eu>
+// SPDX-License-Identifier: MIT
+
+use std::{
+    fmt::{Debug, Display},
+    ops::*,
+};
+
+/// 2 signals into one
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub enum Signal2 {
+    LOLO,
+    LOHI,
+    HILO,
+    HIHI,
+}
+
+impl Default for Signal2 {
+    fn default() -> Self {
+        Self::LOLO
+    }
+}
+
+impl Signal2 {
+    /// All signals `HI`
+    pub const TRUE: Self = Self::HIHI;
+
+    /// All signals `LO`
+    pub const FALSE: Self = Self::LOLO;
+
+    pub fn new(values: i8) -> Self {
+        // Discard non-relevant bits
+        let values = values & 0b11;
+        match values {
+            0 => Self::LOLO,
+            1 => Self::LOHI,
+            2 => Self::HILO,
+            _ => Self::HIHI,
+        }
+    }
+
+    pub fn get(&self, index: usize) -> i8 {
+        assert!(index < 2);
+        let mask = 1 << index;
+        (*self as i8 & mask) >> index
+    }
+}
+
+impl From<i8> for Signal2 {
+    fn from(value: i8) -> Self {
+        Signal2::new(value)
+    }
+}
+
+impl From<u16> for Signal2 {
+    fn from(value: u16) -> Self {
+        Signal2::new(value as i8)
+    }
+}
+
+impl From<i32> for Signal2 {
+    fn from(value: i32) -> Self {
+        Self::from(value as i8)
+    }
+}
+
+impl From<&Signal2> for i8 {
+    fn from(signal: &Signal2) -> Self {
+        *signal as i8
+    }
+}
+
+impl Not for Signal2 {
+    type Output = Signal2;
+
+    fn not(self) -> Self::Output {
+        Signal2::new(!(self as i8))
+    }
+}
+
+impl BitAnd for Signal2 {
+    type Output = Signal2;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        Self::new(self as i8 & rhs as i8)
+    }
+}
+
+impl Debug for Signal2 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{:02b}", *self as i8))
+    }
+}
+
+impl Display for Signal2 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{:02b}", *self as i8))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn run() {
+        let sig = Signal2::new(0b10);
+
+        assert_eq!(sig.get(0), 0);
+        assert_eq!(sig.get(1), 1);
+    }
+}
