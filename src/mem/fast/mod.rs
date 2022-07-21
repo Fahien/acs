1825b5f3d74a13e96e84a89eeeb7813017b8ae43
Antonio Caggiano <info@antoniocaggiano.eu>
2022-07-22 00:45:18 +0200

    mem: Ram8
    Define the Ram8 chip in terms of 8 Register16, a Demux8Way, and a
    Mux8Way16 gate.

--- src/mem/fast/mod.rs
+++ src/mem/fast/mod.rs
@@ -2,14 +2,5 @@
 // Author: Antonio Caggiano <info@antoniocaggiano.eu>
 // SPDX-License-Identifier: MIT
 
-pub mod fast;
-pub mod time;
-pub use time::*;
-pub mod dff;
-pub use dff::*;
-pub mod bit;
-pub use bit::*;
 pub mod register16;
 pub use register16::*;
-pub mod ram8;
-pub use ram8::*;
