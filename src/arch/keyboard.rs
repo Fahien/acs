e677b53d94482b86cfbce4d0c9d6fd523cf4dc69
Antonio Caggiano <info@antoniocaggiano.eu>
2022-07-29 20:04:48 +0200

    arch: Keyboard
    Define a keyboard as a primitive input device.

---
+++ src/arch/keyboard.rs
@@ -0,0 +1,23 @@
+// Copyright © 2022
+// Author: Antonio Caggiano <info@antoniocaggiano.eu>
+// SPDX-License-Identifier: MIT
+
+use crate::{Signal16, Unit};
+
+/// Built-in register chip acting as a keyboard
+#[derive(Default)]
+pub struct Keyboard {
+    out: Signal16,
+}
+
+impl Keyboard {
+    /// Returns the 16-bit character code of the currently pressed key on the
+    /// physical board, or `0` if no key is pressed
+    pub fn out(&self) -> Signal16 {
+        self.out
+    }
+}
+
+impl Unit for Keyboard {
+    fn eval(&mut self) {}
+}
