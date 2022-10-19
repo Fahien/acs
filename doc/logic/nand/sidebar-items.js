928e7d6ae266900b047a35f341e0cd254af01de1
Fahien <Fahien@users.noreply.github.com>
2022-10-19 11:05:08 +0000

    deploy: c7c0eb8c3f5f1f655f50ded3652bed02d6f72660

--- doc/logic/nand/sidebar-items.js
+++ doc/logic/nand/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["Nand",""]]};
+window.SIDEBAR_ITEMS = {"struct":[["Nand","The NAND gate is the only built-in logic component"]]};
--- doc/logic/unit/sidebar-items.js
+++ doc/logic/unit/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"trait":[["Unit",""]]};
+window.SIDEBAR_ITEMS = {"trait":[["Unit","Common operations for a logic unit"]]};
