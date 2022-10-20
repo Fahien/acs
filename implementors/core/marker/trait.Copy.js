7ec022b7ab8440c33ae722b9dd50cd0eee89bd1a
Fahien <Fahien@users.noreply.github.com>
2022-10-20 07:23:25 +0000

    deploy: bfbf173669a88f36d321b75559940537f5a0b7c9

--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["acs"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"acs/logic/signal/struct.Signal.html\" title=\"struct acs::logic::signal::Signal\">Signal</a>","synthetic":false,"types":["acs::logic::signal::Signal"]}];
+implementors["acs"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"acs/logic/signal/enum.Signal.html\" title=\"enum acs::logic::signal::Signal\">Signal</a>","synthetic":false,"types":["acs::logic::signal::Signal"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["acs"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"acs/logic/signal/enum.Signal.html\" title=\"enum acs::logic::signal::Signal\">Signal</a>","synthetic":false,"types":["acs::logic::signal::Signal"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
