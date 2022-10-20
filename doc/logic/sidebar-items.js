1f265c1a80c804eedda96f26856284a8fe4b9815
Fahien <Fahien@users.noreply.github.com>
2022-10-20 10:09:07 +0000

    deploy: d42ab2aca428b51d19732fc2a662708c02a6ac2b

--- doc/logic/sidebar-items.js
+++ doc/logic/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["and",""],["demux",""],["mux",""],["nand",""],["nand16",""],["not",""],["not16",""],["or",""],["signal",""],["signal16",""],["unit",""],["xor",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["and",""],["and16",""],["demux",""],["mux",""],["nand",""],["nand16",""],["not",""],["not16",""],["or",""],["signal",""],["signal16",""],["unit",""],["xor",""]]};
--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"acs":["",[["logic",[],["and.rs","demux.rs","mod.rs","mux.rs","nand.rs","nand16.rs","not.rs","not16.rs","or.rs","signal.rs","signal16.rs","unit.rs","xor.rs"]]],["lib.rs"]]\
+"acs":["",[["logic",[],["and.rs","and16.rs","demux.rs","mod.rs","mux.rs","nand.rs","nand16.rs","not.rs","not16.rs","or.rs","signal.rs","signal16.rs","unit.rs","xor.rs"]]],["lib.rs"]]\
 }');
 createSourceSidebar();
