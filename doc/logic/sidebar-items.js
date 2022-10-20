69294f4555caffe911c43da945e630cf95efeb17
Fahien <Fahien@users.noreply.github.com>
2022-10-20 08:32:35 +0000

    deploy: 50bbb00e9e89b8bf9c715968a723e3e386ef555f

--- doc/logic/sidebar-items.js
+++ doc/logic/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["and",""],["mux",""],["nand",""],["not",""],["or",""],["signal",""],["unit",""],["xor",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["and",""],["demux",""],["mux",""],["nand",""],["not",""],["or",""],["signal",""],["unit",""],["xor",""]]};
--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"acs":["",[["logic",[],["and.rs","mod.rs","mux.rs","nand.rs","not.rs","or.rs","signal.rs","unit.rs","xor.rs"]]],["lib.rs"]]\
+"acs":["",[["logic",[],["and.rs","demux.rs","mod.rs","mux.rs","nand.rs","not.rs","or.rs","signal.rs","unit.rs","xor.rs"]]],["lib.rs"]]\
 }');
 createSourceSidebar();
