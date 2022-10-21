aa646fc384e04ee9a6c622090b662752ae6c34c6
Fahien <Fahien@users.noreply.github.com>
2022-10-21 14:54:26 +0000

    deploy: c99d18b73e469437cd940b87c4947f061a592f72

---
+++ implementors/core/ops/arith/trait.Neg.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["acs"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/ops/arith/trait.Neg.html\" title=\"trait core::ops::arith::Neg\">Neg</a> for <a class=\"struct\" href=\"acs/logic/signal16/struct.Signal16.html\" title=\"struct acs::logic::signal16::Signal16\">Signal16</a>","synthetic":false,"types":["acs::logic::signal16::Signal16"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"acs":["",[["logic",[],["add16.rs","and.rs","and16.rs","demux.rs","demux4way.rs","demux8way.rs","fulladder.rs","halfadder.rs","inc16.rs","mod.rs","mux.rs","mux16.rs","mux4way16.rs","mux8way16.rs","nand.rs","nand16.rs","not.rs","not16.rs","or.rs","or16.rs","or8way.rs","signal.rs","signal16.rs","signal2.rs","signal3.rs","signal8.rs","unit.rs","xor.rs"]]],["lib.rs"]]\
+"acs":["",[["logic",[],["add16.rs","alu.rs","and.rs","and16.rs","demux.rs","demux4way.rs","demux8way.rs","fulladder.rs","halfadder.rs","inc16.rs","mod.rs","mux.rs","mux16.rs","mux4way16.rs","mux8way16.rs","nand.rs","nand16.rs","not.rs","not16.rs","or.rs","or16.rs","or16way.rs","or8way.rs","signal.rs","signal16.rs","signal2.rs","signal3.rs","signal8.rs","unit.rs","xor.rs"]]],["lib.rs"]]\
 }');
 createSourceSidebar();
