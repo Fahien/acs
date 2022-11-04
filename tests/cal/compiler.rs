3677c451596c802d0406a15bcc1011cc652e2be6
Antonio Caggiano <info@antoniocaggiano.eu>
2022-11-04 15:03:09 +0100

    cal: Compiler: Return integer
    Compile and test a function returning an integer.

---
+++ tests/cal/compiler.rs
@@ -0,0 +1,39 @@
+// Copyright © 2022
+// Author: Antonio Caggiano <info@antoniocaggiano.eu>
+// SPDX-License-Identifier: MIT
+
+use acs::{compiler::Compile, error::CalError, Computer};
+
+#[test]
+fn hello_void() -> Result<(), CalError> {
+    let asm_instructions = "fn main() {}".compile()?;
+    let mut computer = Computer::default();
+    computer.set_instructions(asm_instructions);
+    for _ in 0..128 {
+        computer.ticktock();
+    }
+    assert_eq!(computer.get_memory().ram[0], 256);
+    Ok(())
+}
+
+#[test]
+fn return_zero() -> Result<(), CalError> {
+    let asm_instructions = "fn main() -> i16 { 1 }".compile()?;
+    let mut computer = Computer::default();
+    computer.set_instructions(asm_instructions);
+    for _ in 0..128 {
+        computer.ticktock();
+    }
+    assert_eq!(computer.get_memory().ram[0], 257);
+    assert_eq!(computer.get_memory().ram[256], 1);
+
+    let asm_instructions = "fn main() -> i16 { return 2; }".compile()?;
+    let mut computer = Computer::default();
+    computer.set_instructions(asm_instructions);
+    for _ in 0..128 {
+        computer.ticktock();
+    }
+    assert_eq!(computer.get_memory().ram[0], 257);
+    assert_eq!(computer.get_memory().ram[256], 2);
+    Ok(())
+}
