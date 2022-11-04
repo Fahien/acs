dcd63d9a4422648f6cbfcd747acae1f1d2437857
Antonio Caggiano <info@antoniocaggiano.eu>
2022-11-04 12:40:02 +0100

    cal: Generator: Return integer
    Generate VM instructions for a function returning an integer.

---
+++ tests/cal/generator.rs
@@ -0,0 +1,63 @@
+// Copyright © 2022
+// Author: Antonio Caggiano <info@antoniocaggiano.eu>
+// SPDX-License-Identifier: MIT
+
+use acs::{
+    error::CalError,
+    generator::{Generate, Generator},
+    segment::Segment,
+    vm::instruction::VmInstruction,
+};
+
+#[test]
+fn hello_void() -> Result<(), CalError> {
+    let module = "fn main() {}".parse()?;
+    let vm_instructions = Generator::default().gen_module(&module)?;
+    match &vm_instructions[0] {
+        VmInstruction::Function(name, local_count) => {
+            assert_eq!(name, "main");
+            assert_eq!(*local_count, 0);
+        }
+        _ => panic!(),
+    }
+    match &vm_instructions[1] {
+        VmInstruction::Return(return_size_in_words) => {
+            assert_eq!(*return_size_in_words, 0);
+        }
+        _ => panic!(),
+    }
+    Ok(())
+}
+
+#[test]
+fn return_zero() -> Result<(), CalError> {
+    let vm_instructions = "fn main() -> i16 { 0 }".generate()?;
+    match &vm_instructions[0] {
+        VmInstruction::Function(name, local_count) => {
+            assert_eq!(name, "main");
+            assert_eq!(*local_count, 0);
+        }
+        _ => panic!(),
+    }
+    assert!(matches!(
+        vm_instructions[1],
+        VmInstruction::Push(Segment::Constant, 0)
+    ));
+    assert!(matches!(vm_instructions[2], VmInstruction::Return(1)));
+
+    let vm_instructions = "fn main() -> i16 { return 0; }".generate()?;
+    match &vm_instructions[0] {
+        VmInstruction::Function(name, local_count) => {
+            assert_eq!(name, "main");
+            assert_eq!(*local_count, 0);
+        }
+        _ => panic!(),
+    }
+    assert!(matches!(
+        vm_instructions[1],
+        VmInstruction::Push(Segment::Constant, 0)
+    ));
+    assert!(matches!(vm_instructions[2], VmInstruction::Return(1)));
+
+    Ok(())
+}
