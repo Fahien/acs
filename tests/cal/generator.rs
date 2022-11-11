b0b89d72e284e1d8b0f90ad9771f347ea84e337d
Antonio Caggiano <info@antoniocaggiano.eu>
2022-11-11 18:23:42 +0100

    cal: Generator: Define local
    Generate VM instructions for a function defining two local variables.

--- tests/cal/generator.rs
+++ tests/cal/generator.rs
@@ -3,7 +3,10 @@
 // SPDX-License-Identifier: MIT
 
 use acs::{
-    error::CalError, generator::Generator, segment::Segment, vm::instruction::VmInstruction,
+    error::CalError,
+    generator::{Generate, Generator},
+    segment::Segment,
+    vm::instruction::VmInstruction,
 };
 
 #[test]
@@ -58,5 +61,35 @@ fn return_zero() -> Result<(), CalError> {
     ));
     assert!(matches!(vm_instructions[2], VmInstruction::Return(1)));
 
+    Ok(())
+}
+
+#[test]
+fn def_local() -> Result<(), CalError> {
+    let vm_instructions = "fn def_local() { let x: i16 = 0; let y: i16 = 1; }".generate()?;
+    match &vm_instructions[0] {
+        VmInstruction::Function(name, local_count) => {
+            assert_eq!(name, "def_local");
+            assert_eq!(*local_count, 2);
+        }
+        _ => panic!(),
+    }
+    assert!(matches!(
+        vm_instructions[1],
+        VmInstruction::Push(Segment::Constant, 0)
+    ));
+    assert!(matches!(
+        vm_instructions[2],
+        VmInstruction::Pop(Segment::Local, 0)
+    ));
+    assert!(matches!(
+        vm_instructions[3],
+        VmInstruction::Push(Segment::Constant, 1)
+    ));
+    assert!(matches!(
+        vm_instructions[4],
+        VmInstruction::Pop(Segment::Local, 1)
+    ));
+    assert!(matches!(vm_instructions[5], VmInstruction::Return(0)));
     Ok(())
 }
