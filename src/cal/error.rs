f134f672e580a1087c18dcef290eb5b050cc56b2
Antonio Caggiano <info@antoniocaggiano.eu>
2022-11-15 09:12:55 +0100

    cal: Parser: Add two numbers
    Parse code for adding two numbers together.

--- src/cal/error.rs
+++ src/cal/error.rs
@@ -2,6 +2,8 @@
 // Author: Antonio Caggiano <info@antoniocaggiano.eu>
 // SPDX-License-Identifier: MIT
 
+use std::str::FromStr;
+
 use crate::tokenizer::Range;
 
 #[derive(Clone, Debug)]
@@ -13,5 +15,19 @@ pub struct CalError {
 impl CalError {
     pub fn new(message: String, range: Range) -> Self {
         Self { message, range }
+    }
+}
+
+impl From<String> for CalError {
+    fn from(message: String) -> Self {
+        Self::new(message, Range::default())
+    }
+}
+
+impl FromStr for CalError {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(CalError::new(s.into(), Range::default()))
     }
 }
