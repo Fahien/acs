53d59be5b6228db7eea2081f9bb97bbc5a7613e4
Antonio Caggiano <info@antoniocaggiano.eu>
2022-11-15 09:34:12 +0100

    cal: Compiler: Add two numbers
    Compile and test code for adding two numbers together.

--- tests/cal/compiler.rs
+++ tests/cal/compiler.rs
@@ -92,3 +92,16 @@ fn multi_parameters() -> Result<(), CalError> {
     assert_eq!(computer.get_memory().ram[256], 5);
     Ok(())
 }
+
+#[test]
+fn add() -> Result<(), CalError> {
+    let asm_instructions = "fn main() -> i16 { 1 + 2 }".compile()?;
+    let mut computer = Computer::default();
+    computer.set_instructions(asm_instructions);
+    for _ in 0..128 {
+        computer.ticktock();
+    }
+    assert_eq!(computer.get_memory().ram[0], 257);
+    assert_eq!(computer.get_memory().ram[256], 3);
+    Ok(())
+}
