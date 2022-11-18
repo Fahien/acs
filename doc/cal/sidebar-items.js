8c7843384d29b67dc1aa2094355c63a0cac44606
Fahien <Fahien@users.noreply.github.com>
2022-11-18 17:28:56 +0000

    deploy: 1e4977f7af10fdb211425f51964b10f27422015b

--- doc/cal/sidebar-items.js
+++ doc/cal/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["compiler",""],["error",""],["expression",""],["generator",""],["parser",""],["statement",""],["structure",""],["symboltable",""],["tokenizer",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["compiler",""],["error",""],["expression",""],["generator",""],["parser",""],["preamble",""],["statement",""],["structure",""],["symboltable",""],["tokenizer",""]]};
